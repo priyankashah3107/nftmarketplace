99ec8b68431a9610fa5cb78bbf3500a556bdb72f
Priyanka <priyankashah3107@gmail.com>
2024-09-17 19:18:52 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl">
+      Discover digital art & Collect NFTs
+      NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists. 
+      <img src="/magic/mashroom.png" alt=""  className='mashroom_linear' />
+    </h1>
+  )
+}
+
+export default App
